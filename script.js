ae0def8986ba02b3376f75a8419400c977ad1141
AWebster7160 <156373395+AWebster7160@users.noreply.github.com>
2024-02-10 20:47:28 -0500

    Current player and ability to place piece

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-const createGameboard = (function() {
-    const makeBoard = function(grid) {
-        const board = [];
-        for(i=0; i<(grid*grid); i++) {
-            board.push('');
-        }
-        return board;
+function Gameboard(size) {
+    const board = [];
+    for (i = 0; i < size*size; i++) {
+        board.push('');
     }
-    return { makeBoard };
-})();
+    const move = function(index) {
+        if (currentPlayer = '') {
+            currentPlayer = players[0];
+        }
+        if (currentPlayer === players[0]) {
+            board.splice(index, 1, players[0].piece);
+            this.switchPlayer();
+        } else {
+            board.splice(index, 1, players[1].piece);
+            this.switchPlayer();
+        }
+    }
+    let player1;
+    let player2;
+    let players = [
+        {
+            name: player1,
+            piece: 'X'
+        },
+        {
+            name: player2,
+            piece: 'O'
+        }
+    ];
+    const switchPlayer = function() { 
+        players = players.reverse()
+        return players;
+    }
+    return { board, move, players, switchPlayer };
+}
 
+const board1 = Gameboard(3);
